0c555d23fc0cd64698c532d73f127d4fdaff657f
Fermionisme <48383050+endans@users.noreply.github.com>
2026-03-19 14:22:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Dapatkan dari: Supabase Dashboard > Project Settings > API
 // ============================================================
 
-window.SUPABASE_URL = 'https://uzgvhtxqxvliyxrfiauk.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6Z3ZodHhxeHZsaXl4cmZpYXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMDYwNTYsImV4cCI6MjA4NzU4MjA1Nn0.2vy--7dqXM_HbZUbyQ7WizQa3Qs3ahgrbGVd4ZvkwAc';
+window.SUPABASE_URL = 'https://....supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1Ni...';
